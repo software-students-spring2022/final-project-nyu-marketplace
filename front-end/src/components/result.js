d207694e8ffd7893dc6ed9b9886f10be665b134e
Ruochen Miao <rm5327@nyu.edu>
2022-03-18 16:16:54 -0400

    create searchresult page and add search bar

--- front-end/src/components/result.js
+++ front-end/src/components/result.js
@@ -1,11 +1,30 @@
 import "./result.css"
+import Header from "./header"
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
-const resultPage = (props) => {
+const ResultPage = (props) => {
     return (
         <>
-            <h1>result</h1>
+            <Header logged = "True"></Header>
+            <Container fluid><form method = "post" action = "/result">
+                <center><Row>
+                    <Col><input type = "checkbox" name = "academicSupplies" value = "True" /> Academic Supplies</Col>
+                    <Col><input type = "checkbox" name = "dormAppliances" value = "True" /> Dorm Appliances</Col>
+                    <Col><input type = "checkbox" name = "clothing" value = "True" /> Clorhing</Col>
+                    <Col><input type = "checkbox" name = "other" value = "True"/> Other</Col>
+                </Row></center>
+                <center><Row>
+                    <Col xs = {1}></Col>
+                    <Col><input type = "text" name = "searchText"/></Col>
+                    <Col xs = {1}><input type = "submit" value = "Search" /></Col>
+                    <Col xs = {1}></Col>
+                </Row></center>
+            </form></Container>
         </>
     )
 }
 
-export default resultPage
+export default ResultPage
