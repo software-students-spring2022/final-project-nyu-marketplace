7f2efa4a580a9f800dc7d9c3ba3df5808f05a5f4
dleguisamo <david.leguisamo@nyu.edu>
2022-03-20 17:47:32 -0400

    commit to update code on another machine

--- front-end/src/components/LandingPage.js
+++ front-end/src/components/LandingPage.js
@@ -1,15 +1,25 @@
+//Import from dependencies
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Button } from 'react-bootstrap';
 
+//Import styles
 import './LandingPage.css'
+
 const LandingPage = () => {
   return (
 
-  <Container id='container'>
-    <Row id='mainrow'>
-      <div>hello</div>
+  <Container fluid id='container'>
+    <Row>
+      <Col sm={8} id='header'>
+        <div>NYU Marketplace</div>
+      </Col>
+      <Col sm={10} id='header-2'>
+        <Button id='login-button'>Log In</Button>
+      </Col>
     </Row>
+
   </Container>
 
 
