a5c2f92538a8d9c40024101a9c1c7f932a01a123
Rory Jones <rtj230@nyu.edu>
2022-03-20 16:01:06 -0400

    added seller form / manage items components

---
+++ front-end/src/components/SellerForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './header'
+import './SellerForm.css'
+
+const SellerForm = () => {
+    return (
+      <div>
+        <Header></Header>
+        div TEST
+      </div>
+    )
+  }
+
+export default SellerForm
