cc93ff7b0531af517cb7221330d3f5b13ac24126
Ruochen Miao <rm5327@nyu.edu>
2022-03-20 22:23:48 -0400

    make header consistent over pages

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -3,7 +3,10 @@ import LandingPage from './LandingPage'
 import Header from './header'
 import Result from './result'
 import About from './About'
+import ResultPage from './result'
+import SearchBar from './searchBar'
+import ItemLine from './itemLine'
 
 
 // export them
-export { LandingPage, Header, Result, About }
+export { LandingPage, Header, Result, About, ResultPage, SearchBar, ItemLine }
