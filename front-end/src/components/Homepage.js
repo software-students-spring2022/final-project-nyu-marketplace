00d70fd843ea4de767120452d19567144909c911
dleguisamo <david.leguisamo@nyu.edu>
2022-03-24 15:17:01 -0400

    Created backend route for search, axios get requests on frontend

--- front-end/src/components/Homepage.js
+++ front-end/src/components/Homepage.js
@@ -2,14 +2,72 @@ import { Container, Row, Col, Form, FormControl, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Header from './header'
 import {useState} from 'react'
+import axios from 'axios'
 
 import './Homepage.css'
 
 const Homepage = () => {
   const [search, setSearch] = useState();
   const doSearch = () => {
-      console.log('send search to api endpoint')
-  }  
+    axios.get(`http://localhost:3000/search/?q=${search}`)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}  
+
+    const doAcademicSearch = () => {
+      axios.get(`http://localhost:3000/search/?q=academic`)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+
+    const doClothingSearch = () => {
+      axios.get(`http://localhost:3000/search/?q=clothing`)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+
+    const doDormSearch = () => {
+      axios.get(`http://localhost:3000/search/?q=dorm`)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+
+    const doOtherSearch = () => {
+      axios.get(`http://localhost:3000/search/?q=other`)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+
   return (
       <Container fluid id='homepage-container'>
           <Header/>
@@ -19,7 +77,7 @@ const Homepage = () => {
                 <Form id='form'>
                     <FormControl id='searchbar' type='search' placeholder='Start Searching' aria-label='Search' onChange={ e=> setSearch(e.target.value)}/>
                 </Form>
-                <Link to = "/result"><Button id='search-button' variant="outline-secondary">Search</Button></Link>
+                <Button id='search-button' variant="outline-secondary" onClick={doSearch}>Search</Button>
                 <div id='search-title'>Browse By Category</div>
             </Col>
           </Row>
@@ -27,19 +85,19 @@ const Homepage = () => {
           <Row>
             <Col id='categories'>
                 <Link to='/result'>
-                    <Button id='category-button'>Academic Supplies</Button>
+                    <Button id='category-button' onClick={doAcademicSearch}>Academic Supplies</Button>
                 </Link>
                 
                 <Link to='/result'>
-                    <Button id='category-button'>Dorm Appliances</Button>
+                    <Button id='category-button' onClick={doDormSearch}>Dorm Appliances</Button>
                 </Link>
                 
                 <Link to='/result'>
-                    <Button id='category-button'>Clothing</Button>
+                    <Button id='category-button' onClick={doClothingSearch}>Clothing</Button>
                 </Link>
 
                 <Link to='/reuslt'>
-                    <Button id='category-button'>Other</Button>
+                    <Button id='category-button' onClick={doOtherSearch}>Other</Button>
                 </Link>
             </Col>
           </Row>
