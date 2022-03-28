216f9975962b2ad562abf7a2dac1487c41418254
Ruochen Miao <rm5327@nyu.edu>
2022-03-28 17:17:42 -0400

    add searchbar search

--- front-end/src/components/searchBar.js
+++ front-end/src/components/searchBar.js
@@ -3,12 +3,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import { Link } from "react-router-dom";
 
 const SearchBar = (props) => {
     return (
         <>
-            <Container fluid><form method = "post" action = "/result">
+            <Container fluid><form method = "get" action = "/result">
                 <center><Row>
                     <Col><input type = "checkbox" name = "academicSupplies" value = "True" /> Academic Supplies</Col>
                     <Col><input type = "checkbox" name = "dormAppliances" value = "True" /> Dorm Appliances</Col>
@@ -18,7 +17,7 @@ const SearchBar = (props) => {
                 <center><Row>
                     <Col xs = {1}></Col>
                     <Col><input type = "text" name = "searchText"/></Col>
-                    <Col xs = {1}><Link to = "/result"><input type = "submit" value = "Search" /></Link></Col>
+                    <Col xs = {1}><input type = "submit" value = "Search" /></Col>
                     <Col xs = {1}></Col>
                 </Row></center>
             </form></Container>
