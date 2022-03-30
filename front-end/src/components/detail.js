dd05ec8ea7df7b9f2a8f36c963bca292fc4e01ce
dleguisamo <david.leguisamo@nyu.edu>
2022-03-30 17:53:50 -0400

    Implemented confimation modal for item ordering

--- front-end/src/components/detail.js
+++ front-end/src/components/detail.js
@@ -4,65 +4,68 @@ import Item from './Item'
 import Container from 'react-bootstrap/Container'
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Modal from 'react-bootstrap/Modal'
 import { Link } from "react-router-dom";
 import { Button } from 'react-bootstrap';
+import { useState } from "react";
 
-const clickOrder = () => {
-    alert("Success!")
-}
+// const clickOrder = () => {
+//     alert("Success!")
+// }
+
+// const OrderConfirmModal = (props) => {
+
+    
+//     return (
+
+//     )
+// }
 
 const DetailPage = (props) => {
-    
-    const itemsArray = [
-        {
-            title: "Agouti",
-            price: "64.90",
-            description: "Cross-group tertiary application",
-            location: "New York",
-            category: "Clothing",
-        },
-        {
-            title: "Four-striped grass mouse",
-            price: "160.47",
-            description: "Re-contextualized high-level function",
-            location: "Shangai",
-            category: "Academic",
-        },
-        {
-            title: "Crocodile",
-            price: "100",
-            description: "Customer-focused fault-tolerant process improvement",
-            location: "Shangai",
-            category: "Other",
-        },
-        {
-            title: "Bison",
-            price: "87.41",
-            description: "Configurable contextually-based initiative",
-            location: "Abu Dhabi",
-            category: "Academic",
-        },
-        {
-            title: "Lemur",
-            price: "17",
-            description: "Face to face content-based ability",
-            location: "New York",
-            category: "Clothing",
-        },
-        {
-            title: "Tern",
-            price: "499.99",
-            description: "Open-architected zero tolerance alliance",
-            location: "New York",
-            category: "Dorm",
-        }
-    ]
+    const [show, setShow] = useState(false);
+
+
+    const orderItem = () => {
+        //Here would go the route to order the item
+        console.log("Tasks:\nAdd item to user info in database\nRemove item from public view")
+    }
 
     return (
         <>
+            <Modal
+                {...props}
+                backdrop="static"
+                keyboard={false}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                show={show} 
+                onHide={() => setShow(false)}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <div id='modal-header-text'>Order Confirmation</div> 
+                    </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body id='modal-body-text'>
+                    <div id='modal-body'>
+                        <div>Hello, please review the following before reserving this item:<br/>
+                            <br/>After reservation, you must contact the seller within 24 hours in order to set up a meeting time and place. Please
+                            note that the meeting location must be within AN NYU FACILITY. 
+                            <br/>After the item is exchanged, and the payment has been made, you must mark the order as completed -- which can be done
+                            by accessing the item on your homepage. 
+                        </div>
+                        <Button id='modal-reserve-button' onClick={orderItem}>Reserve Item</Button>
+                    </div>
+                </Modal.Body>
+            </Modal>
+
+
             <Header logged="True"/>
             <center>
-            <Item 
+            <Item
+                // Replace with data recovered from API call 
                 title = "Amazing Item"
                 price = "10"
                 description = "Amazing!"
@@ -72,8 +75,8 @@ const DetailPage = (props) => {
             <br/>
             <Container>
                 <Row>
-                    <Col><Link to = "/homepage"><Button onClick={clickOrder}>Order</Button></Link></Col>
-                    <Col><Link to = "/homepage"><Button onClick={clickOrder}>Favorite</Button></Link></Col>
+                    <Col><Button onClick={() => setShow(true)}>Order</Button></Col>
+                    <Col><Button>Favorite</Button></Col>
                 </Row>
             </Container>
             </center>
