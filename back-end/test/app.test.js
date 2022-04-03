b9531f24724c17fe8801ccdbe3cebdeb1fd16b17
Clare <pr1532@nyu.edu>
2022-04-03 19:28:46 -0400

    add test for detail route

--- back-end/test/app.test.js
+++ back-end/test/app.test.js
@@ -59,8 +59,19 @@ describe('User routes', () => {
                 });
         });
     });
-    
-
-
-
 });
+
+describe("Detail route", () => {
+    it('should return the item details for a specified id', (done) => {
+        chai.request(app) 
+            .get('/detail?id=dfde3ffa-b68d-407a-b317-e76dd93d2ff4')
+            .end((err, res) => {
+                if (err) done(err)
+                chai.expect(res.status).to.equal(200);
+                chai.expect(res.body).to.be.an('array');
+                chai.expect(res.body[0]._id).to.equal('dfde3ffa-b68d-407a-b317-e76dd93d2ff4');
+                chai.expect(res.body[0]).to.have.property('contact');
+                done();
+            })
+    })
+})
