07574497661f988be4ff6d23b266868693622f6a
Ruochen Miao <rm5327@nyu.edu>
2022-04-27 15:58:25 -0400

    fix searchbar on 'result' and 'favorites'

--- front-end/src/components/searchBar.js
+++ front-end/src/components/searchBar.js
@@ -3,21 +3,39 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = (props) => {
+
+    const [category, setCategory] = useState('');
+    const [keyword, setKeyword] = useState('');
+    const navigate = useNavigate();
+
     return (
         <>
             <Container fluid><form method = "get" action = "/result">
                 <center><Row>
-                    <Col><input type = "radio" name = "category" value = "Academic" /> Academic Supplies</Col>
-                    <Col><input type = "radio" name = "category" value = "Dorm" /> Dorm Appliances</Col>
-                    <Col><input type = "radio" name = "category" value = "Clothing" /> Clorhing</Col>
-                    <Col><input type = "radio" name = "category" value = "Other"/> Other</Col>
+                    <Col><input type = "radio" checked={category === 'Academic'} onClick={() => {setCategory('Academic')}}/> Academic Supplies</Col>
+                    <Col><input type = "radio" checked={category === 'Dorm'} onClick={() => {setCategory('Dorm')}}/> Dorm Appliances</Col>
+                    <Col><input type = "radio" checked={category === 'Clothing'} onClick={() => {setCategory('Clothing')}}/> Clorhing</Col>
+                    <Col><input type = "radio" checked={category === 'Other'} onClick={() => {setCategory('Other')}}/> Other</Col>
                 </Row></center>
                 <center><Row>
                     <Col xs = {1}></Col>
-                    <Col><input type = "text" name = "searchText"/></Col>
-                    <Col xs = {1}><input type = "submit" value = "Search" /></Col>
+                    <Col><input type = "text" name = "searchText" value={keyword} onChange={e => {setKeyword(e.target.value)}}/></Col>
+                    <Col xs = {1}><input type = "submit" value = "Search" onClick={e => {
+                        e.preventDefault();
+                        if (category === ''){
+                            setCategory('')
+                            setKeyword('')
+                            navigate(`/result?searchText=${keyword}`);
+                        } else {
+                            setCategory('')
+                            setKeyword('')
+                            navigate(`/result?category=${category}&searchText=${keyword}`)
+                        }
+                    }}/></Col>
                     <Col xs = {1}></Col>
                 </Row></center>
             </form></Container>
